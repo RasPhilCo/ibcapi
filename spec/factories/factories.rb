e5822f09124439492a48ac21b7662f3f3dae717d
RasPhilCo <Ras@PhilCo>
2014-09-18 18:23:45 -0400

    Fix faker/factory bug

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -2,13 +2,12 @@ require 'faker'
 
 FactoryGirl.define do 
   factory :video do 
-    @id = Faker::Lorem.characters(10)
-     yt_id @id
-     title Faker::Lorem.sentence
-     description Faker::Lorem.paragraph
-     player_url Faker::Internet.url('interwebs.com', "/embed/#{@id}")
-     view_count Faker::Number.number(5)
-     favorite_count Faker::Number.number(3)
-     popularity Faker::Number.number(2)
+     yt_id { @id = Faker::Lorem.characters(10) }
+     title { Faker::Lorem.sentence }
+     description { Faker::Lorem.sentence }
+     player_url { Faker::Internet.url('interwebs.com', "/embed/#{@id}") }
+     view_count { Faker::Number.number(5) }
+     favorite_count { Faker::Number.number(3) }
+     popularity { Faker::Number.number(2) }
   end 
 end
